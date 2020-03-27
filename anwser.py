993f06f9d0be97e3ea949d68476af1d7f2769426
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-03-26 20:14:11 -0500

    Se agrega condicionales para identifcar que el peso del producto este en el ragno permitido, identificar el tipo de vuelo, aplicar descuentos en caso de ser necesario y la impresion del valor total del envio

--- anwser.py
+++ anwser.py
@@ -9,6 +9,24 @@ pa = 0
 d=float(input("Ingrese la distancia a recorrer: "))
 v=int(input("Escoja un tipo de vuelo: Nacional(1) o Internacional(2)"))
 
-if pa<=337250 :
+while pa<=337250 :
     p=float(int("Ingrese el peso del producto: "))
-    
+    if p>10 :
+        pa=pa+p
+        if pa<=355000 :
+            if v==1 or v==2 :
+                if v==1 :
+                    vd=4000*d
+                    vp=4500*p
+                    vt=vd+vp
+                    if p>100 :
+                        vt=vt*0.85
+                elif v==2:
+                    vd=8000*d
+                    vp=4500*p
+                    vt=vd+vp
+                    if pa>400 and d>8000 :
+                        vt=vt*0.90
+                print("El valor total es: " + str(vt))
+            else :
+                
