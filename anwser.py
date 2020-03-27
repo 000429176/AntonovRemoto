01465621c4070ba9c16120fcc24249981a025bd6
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-03-26 20:40:03 -0500

    Se completo el ciclo while

--- anwser.py
+++ anwser.py
@@ -13,7 +13,7 @@ while not(v==1) and not(v==2) :
     v=int(input("Ingrese un número válido "))
 
 while pa<=337250 :
-    p=float(int("Ingrese el peso del producto: "))
+    p=float(input("Ingrese el peso del producto: "))
     if p>10 :
         pa=pa+p
         if pa<=355000 :   
@@ -30,4 +30,14 @@ while pa<=337250 :
                 if pa>400 and d>8000 :
                     vt=vt*0.90
             print("El valor total es: " + str(vt))
+        else : 
+            print("El paquete supera el límite de peso")
+            pa=pa-p
+    else :
+        print("El peso de su producto no es aceptado")
+    d=float(input("Ingrese una nueva distancia: "))
+    
+
+    
+            
             
