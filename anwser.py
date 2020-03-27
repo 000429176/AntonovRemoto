9308304a91fdf16cf08ccf06d10352bff45b1484
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-03-26 19:52:23 -0500

    Se agregan valores de entrada

--- anwser.py
+++ anwser.py
@@ -5,4 +5,7 @@ Created on Thu Mar 26 19:19:28 2020
 @author: Omar
 """
 
+pa = 0
+d=float(input("Ingrese la distancia a recorrer: "))
+v=int(input("Escoja un tipo de vuelo: Nacional(1) o Internacional(2)"))
 
