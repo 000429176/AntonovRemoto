3e3628dd5099adf1ca357cd88e9462973276980f
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-03-26 21:00:25 -0500

    Se corrigieron errores en el calculo del valor del envio, se corrigieron los valores de entrada y se agregaron las impresiones finales

--- anwser.py
+++ anwser.py
@@ -10,10 +10,10 @@ d=float(input("Ingrese la distancia a recorrer: "))
 v=int(input("Escoja un tipo de vuelo: Nacional(1) o Internacional(2) "))
 
 while not(v==1) and not(v==2) :
-    v=int(input("Ingrese un número válido "))
+    v=int(input("Escoja 1 o 2 "))
 
 while pa<=337250 :
-    p=float(input("Ingrese el peso del producto: "))
+    p=float(input("Ingrese el peso del producto en kilogramos: "))
     if p>10 :
         pa=pa+p
         if pa<=355000 :   
@@ -27,7 +27,7 @@ while pa<=337250 :
                 vd=8000*d
                 vp=4500*p
                 vt=vd+vp
-                if pa>400 and d>8000 :
+                if p>400 and d>8000 :
                     vt=vt*0.90
             print("El valor total es: " + str(vt))
         else : 
@@ -35,8 +35,16 @@ while pa<=337250 :
             pa=pa-p
     else :
         print("El peso de su producto no es aceptado")
-    d=float(input("Ingrese una nueva distancia: "))
+        
     
+    d=float(input("Ingrese una nueva distancia: "))
+    v=int(input("Escoja un tipo de vuelo: Nacional(1) o Internacional(2) "))
+
+    while not(v==1) and not(v==2) :
+        v=int(input("Escoja 1 o 2 "))
+        
+print("Avion con carga maxima y listo para despegar")
+print("El peso de los paquetes es: " + str(pa))   
 
     
             
