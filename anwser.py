632d9fb7cedff0a01b5599f60e6e86b8357d5f3a
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-03-26 20:26:12 -0500

    Se corrigio la seleccion del tipo de vuelo

--- anwser.py
+++ anwser.py
@@ -7,26 +7,27 @@ Created on Thu Mar 26 19:19:28 2020
 
 pa = 0
 d=float(input("Ingrese la distancia a recorrer: "))
-v=int(input("Escoja un tipo de vuelo: Nacional(1) o Internacional(2)"))
+v=int(input("Escoja un tipo de vuelo: Nacional(1) o Internacional(2) "))
+
+while not(v==1) and not(v==2) :
+    v=int(input("Ingrese un número válido "))
 
 while pa<=337250 :
     p=float(int("Ingrese el peso del producto: "))
     if p>10 :
         pa=pa+p
-        if pa<=355000 :
-            if v==1 or v==2 :
-                if v==1 :
-                    vd=4000*d
-                    vp=4500*p
-                    vt=vd+vp
-                    if p>100 :
-                        vt=vt*0.85
-                elif v==2:
-                    vd=8000*d
-                    vp=4500*p
-                    vt=vd+vp
-                    if pa>400 and d>8000 :
-                        vt=vt*0.90
-                print("El valor total es: " + str(vt))
-            else :
-                
+        if pa<=355000 :   
+            if v==1 :
+                vd=4000*d
+                vp=4500*p
+                vt=vd+vp
+                if p>100 :
+                    vt=vt*0.85
+            elif v==2:
+                vd=8000*d
+                vp=4500*p
+                vt=vd+vp
+                if pa>400 and d>8000 :
+                    vt=vt*0.90
+            print("El valor total es: " + str(vt))
+            
